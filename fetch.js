7aabbbb35589f0872e86f9af2f423a4368f2078e
Sean Porter <por22009@byui.edu>
2024-02-06 13:42:20 -0700

    w05 learning activities

---
+++ fetch.js
@@ -0,0 +1,42 @@
+//fetch.js
+const url = "https://pokeapi.co/api/v2/pokemon/ditto";
+const urlList = "https://pokeapi.co/api/v2/pokemon";
+let results = null;
+
+function compare(a, b) {
+    if (a.name > b.name) {
+      return 1;
+    } else if (a.name < b.name) {
+
+      return -1;
+    } else return 0;
+  }
+
+async function getPokemon(url, doThis) {
+  const response = await fetch(url);
+  if (response.ok) {
+    const data = await response.json();
+    doThis(data);
+  }
+}
+
+function doStuff(data) {
+  results = data;
+  const outputElement = document.querySelector("#output");
+  const html = `<h2>${data.name}</h2><img src="${data.sprites.front_default}" alt="${data.name}">`;
+  outputElement.innerHTML = html;
+  console.log("first: ", results);
+}
+
+function doStuffList(data) {
+  console.log(data);
+  const pokeListElement = document.querySelector("#outputList");
+  const pokeList = data.results;
+  pokeList.forEach((currentItem) => {
+    const html = `<li data-url="${item.url}">${currentItem.name}</li>`;
+    pokeListElement.innerHTML += html;
+  });
+}
+getPokemon(url, doStuff);
+console.log("second: ", results);
+getPokemon(urlList, doStuffList);
